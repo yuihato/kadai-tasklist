b0b57259fc38ecf5dc81796154d649abac1f36ef
yui.hattori <totetetote@gmail.com>
2021-04-04 13:04:41 +0000

    user signup

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,17 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in, only: [:show, :index]
+  before_action :correct_user, only: [:destroy, :show]
 
   def index
     @tasks = Task.order(id: :desc).page(params[:page]).per(10)
+      if logged_in?
+      @task = current_user.tasks.build  # form_with 用
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+    end
   end
 
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,24 +19,26 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-
+    @task = current_user.tasks.build(task_params)
     if @task.save
-      flash[:success] = 'Task が正常に投稿されました'
-      redirect_to @task
+      flash[:success] = 'メッセージを投稿しました。'
+      redirect_to root_url
     else
-      flash.now[:danger] = 'Task が投稿されませんでした'
-      render :new
+      @tasks = current_user.tasks.order(id: :desc).page(params[:page])
+      flash.now[:danger] = 'メッセージの投稿に失敗しました。'
+      render 'tasks/index'
     end
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+    @task = Task.find(params[:id])
     if @task.update(task_params)
       flash[:success] = 'Task は正常に更新されました'
-      redirect_to @task
+      redirect_to root_url
     else
       flash.now[:danger] = 'Task は更新されませんでした'
       render :edit
@@ -38,10 +46,11 @@ class TasksController < ApplicationController
   end
 
   def destroy
+    @task = Task.find(params[:id])
     @task.destroy
 
-    flash[:success] = 'task は正常に削除されました'
-    redirect_to tasks_url
+    flash[:success] = 'タスクを削除しました'
+    redirect_to root_url
   end
 
   private
@@ -53,4 +62,11 @@ class TasksController < ApplicationController
   def task_params
     params.require(:task).permit(:content, :status)
   end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
+  end  
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,12 @@ class UsersController < ApplicationController
     end
   end
   
+  def show
+    @user = User.find(params[:id])
+    @tasks = @user.tasks.order(id: :desc).page(params[:page])
+    counts(@user)
+  end
+  
   def new
     @user = User.new
   end
